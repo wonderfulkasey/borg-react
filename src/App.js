4a31248278830285b16338ebb031b6b98ced2fc6
wonderfulkasey <kaseyarnold15@gmail.com>
2021-01-20 09:56:58 -0500

    added navbar page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './Navbar.js'
 
 function App() {
   return (
@@ -9,6 +10,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>
+          hello 
+        </p>
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
